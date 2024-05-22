b7c6d3dea82854d300ebdebf9e9f79d878599a62
Mibo <meebsawosika@gmail.com>
2024-05-21 22:08:24 -0600

    Complete the second exercise of java exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    let repeatedString = string;
+    if(num < 0) {
+        return "ERROR";
+    } else if (num === 0){
+        return "";
+    }else {
+        for(let i = 0; i < num-1; i++){
+            repeatedString += string;
+        }
+        return repeatedString;
+    }    
 };
 
 // Do not edit below this line
