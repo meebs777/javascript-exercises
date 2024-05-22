83a3d500f588010a52e404cd6551c8e5d20ecfd4
Mibo <meebsawosika@gmail.com>
2024-05-22 16:22:12 -0600

    Completed exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  let conversion = (temperature - 32) * 5/9;
+  if(conversion % 1 !== 0)return parseFloat(conversion.toFixed(1));
+  else return conversion
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  let conversion = temperature * 9/5 + 32;
+  if(conversion % 1 !== 0)return parseFloat(conversion.toFixed(1));
+  else return conversion
 };
 
 // Do not edit below this line
