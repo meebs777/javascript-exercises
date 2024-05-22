7fe7d21ad2d88c5eac569c818ba77a973be7a954
Mibo <meebsawosika@gmail.com>
2024-05-21 23:28:20 -0600

    Completed reverse a string function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let length = string.length;
+    const reverse = [];
 
+    for (let i = 0; i <= length; i++ ){
+        reverse[i] = string.at(length-i);
+    }
+
+    return reverse.join("")
 };
 
 // Do not edit below this line
