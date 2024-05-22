10e9a71812774126dfc60b377bd0ab1b891a1c6e
Mibo <meebsawosika@gmail.com>
2024-05-22 00:04:53 -0600

    Complete the removeFromArray functions

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...match) {
+    let arrayLength = array.length;
+    let matchLength = match.length;
 
+    for(let i = 0; i < matchLength; i++){
+        for(let j = 0; j < arrayLength; j++){
+            if (array[j] === match[i]){
+                array.splice(j,1);
+                j--;
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
