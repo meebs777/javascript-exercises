d04bd2d409825e9b48a8cbd988a86621f77d26c8
Mibo <meebsawosika@gmail.com>
2024-05-29 22:17:58 -0600

    Completed exercise 8, calculator functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((sum,item) => {
+    return sum += item;
+  },0 )
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((sum,item) => {
+    return sum *= item;
+  } )
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let factor = 1;
+  
+  for(let i = 1; i <= num; i++) {
+    factor *= i;
+  }
+
+  return factor;
 };
 
 // Do not edit below this line
