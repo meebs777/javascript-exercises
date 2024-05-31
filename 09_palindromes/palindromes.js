d25892c6f798a357b3afd33482e4e94e2da473a5
Mibo <meebsawosika@gmail.com>
2024-05-30 20:47:23 -0600

    Completed palindrome js exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    //create copy of string, remove spaces, join back from array to string, seperate back into array now without spaces
+    let reverseArray = string.slice().split(" ").join("").split("").reverse();
+    let forwardArray = string.slice().split(" ").join("").split("");
+    //remove any form of punctuation in strings
+    reverseArray = reverseArray.filter(str => !str.match(/[^\w\s]/g));
+    forwardArray = forwardArray.filter(str => !str.match(/[^\w\s]/g));
+    //make all same case
+    reverseArray = reverseArray.map(str => str.toLowerCase());
+    forwardArray = forwardArray.map(str => str.toLowerCase());
+    
+    for(let i = 0; i < string.length; i++) {
+        if (reverseArray[i] === forwardArray[i]) {
+            continue;
+        } else {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
