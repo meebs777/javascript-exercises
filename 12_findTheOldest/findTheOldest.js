ee17d1ad13825e9dcbc2ccb4bce98813940fb813
Mibo <meebsawosika@gmail.com>
2024-06-02 01:51:22 -0600

    Completed the find the oldest js exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,27 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    const date = new Date();
+    let oldestIndex;
+    console.log(arr);
 
+    const ageDifference = arr.map (people => {
+        if (people.yearOfDeath === undefined) {
+            return date.getFullYear() - people.yearOfBirth;
+        } else {
+            return people.yearOfDeath - people.yearOfBirth;
+        }
+    })
+
+
+    for (let i = 0; i < ageDifference.length; i++) {
+        if (i === 0) {
+            oldestIndex = i;
+            continue;
+        } else if (ageDifference[i] > ageDifference[i-1]) {
+            oldestIndex = i;
+        }
+    }
+    
+    return arr[oldestIndex];
 };
 
 // Do not edit below this line
